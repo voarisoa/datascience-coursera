e9fe9010afb518f7ac7b85074dcae57345b09c7d
voarisoa <61099912+voarisoa@users.noreply.github.com>
2020-04-11 14:04:04 -0600

    Assignment 1 - part 2

---
+++ complete.R
@@ -0,0 +1,27 @@
+complete <- function(directory, id = 1:332){
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+  
+  ## 'id' is an integer vector indicating the monitor ID numbers
+  ## to be used
+  
+  ## Return a data frame of the form:
+  ## id nobs
+  ## 1  117
+  ## 2  1041
+  ## ...
+  ## where 'id' is the monitor ID number and 'nobs' is the
+  ## number of complete cases
+  
+  allfile <- list.files(path = directory, full.names = TRUE)
+  data <- data.frame()
+  nobs <- data.frame()
+  completecases <- data.frame()
+  for (i in id){
+    data <- read.csv(allfile[i], header = TRUE)
+    nobs <- sum(complete.cases(data))
+    completecases <- rbind(completecases, data.frame(i,nobs))
+    
+  }
+  completecases
+}
